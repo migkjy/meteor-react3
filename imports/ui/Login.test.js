df4e11dd2f79e7458dcb5c6914f937a84305a73b
migkjy <migkjy@naver.com>
2017-12-14 04:25:31 -0800

    new start

---
+++ imports/ui/Login.test.js
@@ -0,0 +1,23 @@
+import { Meteor } from 'meteor/meteor';
+import React from 'react';
+import expect from 'expect';
+import { mount } from 'enzyme';
+
+import { Login } from './Login';
+
+if (Meteor.isClient) {
+  describe('Login', function () {
+
+    it('should show error messages', function () {
+      const error = 'This is not working';
+      const wrapper = mount(<Login loginWithPassword={() => {}}/>);
+
+      wrapper.setState({ error });
+      expect(wrapper.find('p').text()).toBe(error);
+
+      wrapper.setState({ error: '' });
+      expect(wrapper.find('p').length).toBe(0);
+    });
+
+  });
+}
